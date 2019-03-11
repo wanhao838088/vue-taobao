7180b604131f046ef4d5b60af341d07843354d20
wanhao <838088516@qq.com>
2019-03-11 17:43:24 +0800

    bottom-bar静态添加

--- renren-vue/src/components/Goods/Goods.vue
+++ renren-vue/src/components/Goods/Goods.vue
@@ -68,9 +68,29 @@
           立即秒杀
         </el-button>
       </div>
-      <!--<div class="main-bottom">-->
 
-      <!--</div>-->
+
+      <div class="bottom-bar">
+        <div class="btn-shop">
+          <i class="iconfont icon-dianpu"></i><span class="btn-text">店铺</span>
+        </div>
+        <div class="btn-wangwang">
+          <i class="iconfont icon-kefu"></i>
+          <span class="btn-text">客服</span>
+        </div>
+
+        <div class="btn-fav">
+          <i class="iconfont icon-shoucang"></i>
+          <span class="btn-text">收藏</span>
+        </div>
+        <div class="bottom-bar-btn cart ">
+          <span class="btn-title">加入购物车</span><!-- empty -->
+        </div>
+
+        <div class="bottom-bar-btn buy ">
+          <span class="btn-title">立即购买</span><!-- empty -->
+        </div>
+      </div>
 
 
 
@@ -292,10 +312,45 @@
       .goods-img
         width 100%
         height 9.5rem;
-    .main-bottom
-      position: absolute;
-      bottom: 1px;
-      height 1rem;
-      width 100%;
-      background-color red;
+    .bottom-bar
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      color white;
+      background-color: #fff;
+      height 1.6rem;
+      display: flex;
+      z-index: 110;
+      flex-direction: row;
+      align-items: center;
+      box-shadow: rgba(0, 0, 0, 0.15) 0 -0.01rem 0.06rem 0;
+      padding-left: 0.1rem;
+      padding-right: 0.1rem;
+      padding-bottom: env(safe-area-inset-bottom);
+      div
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 1rem;
+        height: 1rem;
+        font-size 0.37rem;
+        white-space: nowrap;
+        align-items: center;
+        color: #999;
+        padding: 0 0.1rem;
+      .iconfont
+        font-size 0.66rem;
+      .bottom-bar-btn
+        width: 2.97rem;
+        font-weight: bold;
+        color #fff;
+        height: 1.1rem;
+        text-align center;
+        border-radius: 0.8rem 0 0 0.8rem;
+        background-image: -webkit-linear-gradient(left, #FFC500, #FF9402);
+        background-image: linear-gradient(to right, #FFC500, #FF9402)
+      .buy
+        border-radius: 0 0.6rem 0.6rem 0;
+        background-image: -webkit-linear-gradient(left, #FF7A00, #FE560A);
+        background-image: linear-gradient(to right, #FF7A00, #FE560A);
 </style>
